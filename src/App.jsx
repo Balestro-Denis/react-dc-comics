b4f65900904ea39492e9ebd677765b31ffa1add0
Balestro-Denis <denisbalestro@gmail.com>
2026-02-10 18:02:58 +0100

    Main Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import ComicsList from "./components/ComicsList";
-
+import BlueBar from "./components/Bluebar";
 import "./App.css";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <main>
         <div className="jumbotron"></div>
         <ComicsList />
+        <BlueBar />
       </main>
     </>
   );
--- src/components/BlueBar.jsx
+++ src/components/BlueBar.jsx
@@ -1,11 +1,30 @@
-import "/BlueBar.css";
+import "./BlueBar.css";
 function BlueBar() {
   return (
     <section className="blue-bar">
       <ul className="blue-bar-list">
         <li>
-          <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+          <img
+            src="public/img/buy-comics-digital-comics.png"
+            alt="Digital Comics"
+          />
           <span>DIGITAL COMICS</span>
+        </li>
+        <li>
+          <img src="/img/buy-comics-merchandise.png" alt="Merchandise" />
+          <span>DC MERCHANDISE</span>
+        </li>
+        <li>
+          <img src="/img/buy-comics-shop-locator.png" alt="Shop Locator" />
+          <span>SHOP LOCATOR</span>
+        </li>
+        <li>
+          <img src="/img/buy-comics-subscriptions.png" alt="Subscriptions" />
+          <span>SUBSCRIPTIONS</span>
+        </li>
+        <li>
+          <img src="/img/buy-dc-power-visa.svg" alt="Power Visa" />
+          <span>POWER VISA</span>
         </li>
       </ul>
     </section>
